b1068f8fa023b7f1c8fa9df1b3a398dc86f7dcfa
haocongshun <haocongshun@hikvision.com.cn>
2020-05-12 15:25:56 +0800

    [MOD]修改图像压缩工具代码

--- src/main/java/site/teamo/magic/biu/util/PhotoCompress.java
+++ src/main/java/site/teamo/magic/biu/util/PhotoCompress.java
@@ -9,21 +9,33 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class PhotoCompress {
+    private static String inputRootPath = "D:\\备份\\照片";
+    private static String outputRootPath="D:\\备份\\照片压缩";
+
     public static void main(String[] args) {
-        String outPath = "D:\\备份\\照片压缩";
-        String inputPath ="D:\\备份\\照片";
-        List<String> outFile = Arrays.asList(new File(outPath).listFiles()).stream().map(x->x.getName()).collect(Collectors.toList());
-        Arrays.asList(new File(inputPath).listFiles()).stream().filter(x -> x.isFile()&&!x.getName().endsWith(".mp4")&&!outFile.contains(x.getName()))
+        Arrays.asList(new File(inputRootPath).listFiles()).stream().filter(File::isDirectory).forEach(x -> {
+            handle(x.getAbsolutePath());
+                }
+        );
+    }
+
+    public static void handle(String path){
+        String outputPath = path.replace(inputRootPath,outputRootPath);
+        File[] of = new File(outputPath).listFiles();
+        List<String> outputFile = Arrays.asList(of==null?new File[0]:of).stream().map(x -> x.getName()).collect(Collectors.toList());
+        Arrays.asList(new File(path).listFiles()).stream().filter(x -> x.isFile() && !x.getName().endsWith(".mp4") && !outputFile.contains(x.getName()))
                 .forEach(x -> {
                             System.out.println(x.getName() + ":" + x.length());
-                            compress(outPath,x);
+                            compress(outputPath, x);
                         }
                 );
     }
 
-    public static void compress(String outputPath,File x){
+    public static void compress(String outputPath, File x) {
+        if(!new File(outputPath).exists()){
+            new File(outputPath).mkdirs();
+        }
         try {
-
             if (x.length() > 10000000) {
                 Thumbnails.of(x.getAbsolutePath()).scale(0.4f).toFile(outputPath + File.separator + x.getName());
             } else if (x.length() > 6000000) {
@@ -37,7 +49,7 @@ public class PhotoCompress {
             } else {
                 Thumbnails.of(x.getAbsolutePath()).scale(1.0f).toFile(outputPath + File.separator + x.getName());
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
