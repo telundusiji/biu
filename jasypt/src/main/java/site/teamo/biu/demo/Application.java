4263e829ff7b7d74933028de4e5ceefc9279e1c8
telundusiji <470893279>
2020-08-26 16:19:42 +0800

    添加测试代码

--- jasypt/src/main/java/site/teamo/biu/demo/Application.java
+++ jasypt/src/main/java/site/teamo/biu/demo/Application.java
@@ -1,5 +1,7 @@
 package site.teamo.biu.demo;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.apache.ibatis.session.SqlSession;
 import org.jasypt.encryption.StringEncryptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.*;
@@ -8,11 +10,16 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+
 /**
  * @author 爱做梦的锤子
  * @create 2020/8/24
  */
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class})
+@SpringBootApplication
+@EnableEncryptableProperties
 public class Application implements ApplicationRunner {
 
     public static void main(String[] args) {
@@ -25,11 +32,20 @@ public class Application implements ApplicationRunner {
     @Autowired
     private StringEncryptor encryptor;
 
+    @Autowired
+    private DataSource dataSource;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        String password = "123456";
+
+        ResultSet show_tables = dataSource.getConnection().createStatement().executeQuery("show tables");
+
+        show_tables.first();
+
+        String password = "jpress";
         String encryptPwd = encryptor.encrypt(password);
+
         System.out.println("加密:：" + encryptPwd);
-        System.out.println("解密：" + encryptor.decrypt(encryptPwd));
+//        System.out.println("解密：" + encryptor.decrypt(encryptPwd));
     }
 }
